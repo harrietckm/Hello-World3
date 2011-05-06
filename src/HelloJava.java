2cec4c33ba707a54fea91b18f27d251414a55c48
Harriet King <hcking@mtu.edu>
2011-05-06 15:41:47 -0400

    bin

---
+++ src/HelloJava.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class HelloJava {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+        System.out.println("I'm here in linux");
+        String name = "Pig";
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println(name + ", please enter your name... ");
+
+        name = inp.nextLine();
+        System.out.println("Thank you " + name );
+
+    }
+
+
+}
